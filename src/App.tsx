4b2368d8a8ad1816a62cfdf0da4528ae8a49fd03
Tobias Merkle <tgamerkle@gmail.com>
2022-10-18 16:33:50 -0400

    add a test page for OAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './views/Home'
+import Authorize from './views/Authorize'
 import Podcast from './views/Podcast'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/age' element={<Podcast />} />
+        <Route path='/authorize' element={<Authorize />} />
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/views/Authorize.tsx
@@ -0,0 +1,9 @@
+import Text from '../components/text/Text'
+
+const Authorize = () => {
+  return (
+    <Text>You are on the Authorize page.</Text>
+  )
+}
+
+export default Authorize
