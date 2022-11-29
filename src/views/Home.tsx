e592d773f44460493d676ecbd5f50e99711d9e55
Tobias Merkle <tgamerkle@gmail.com>
2022-11-29 15:34:18 -0500

    dev docs link

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -80,7 +80,7 @@ const Home  = () => {
                     <Divider />
                     <a target='_blank' href='https://uqbarnetwork.medium.com/zk-execution-and-uqbar-a8f49784155e'>ZK-ROLLUPS</a>
                     <Divider />
-                    <a target='_blank' href='https://uqbar-network.gitbook.io/uqbar-developer-documentation/'>DEV DOCS</a>
+                    <a target='_blank' href='https://uqbar-network.gitbook.io/dev-docs/'>DEV DOCS</a>
                   </Entry>}
                   {page == 'blog' && <Entry className='mt1' title='CONTENT'>
                     <a target='_blank' href='https://mirror.xyz/0xE030ad9751Ca3d90D4E69e221E818b41146c2129'>Blog</a>
