2a1e70e92a222406fd0f35126dfc88d9c7841f95
Tobias Merkle <tgamerkle@gmail.com>
2022-12-22 13:04:37 -0500

    oops, git add

---
+++ src/components/phonebook/RedirectToWaitlist.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+const RedirectToWaitlist = () => {
+  useEffect(() => {
+    window.location.replace("https://forms.gle/GGRTECQrVVV2z2ZE9")
+  }, [])
+
+  return <></>
+}
+
+export default RedirectToWaitlist
