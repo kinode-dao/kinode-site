7468d8c48b09a6c00c761330ad9c2efa79f38ec5
Tobias Merkle <tobias@branchmessenger.com>
2023-12-19 10:57:05 -0500

    key in process

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function authenticateToken(req, res, next) {
 
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, SECRET_KEY, (err, user) => {
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
