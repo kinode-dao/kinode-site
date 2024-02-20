8bf83723ccaed6f79ea823b2e49f3db64580c484
Tobias Merkle <tobias@branchmessenger.com>
2024-02-20 14:15:00 -0500

    static 2

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if (isProd) {
 
 const upload = multer({ dest: isProd ? 'public/images/' : 'test/images' });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/', express.static(path.join(__dirname, isProd ? 'public' : 'test')));
 app.use(express.json())
 const db = new sqlite3.Database(isProd ? './db.sqlite' : './db.test.sqlite')
 
