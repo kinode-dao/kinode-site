af46316a62222b4897db8fd0b3afe8ce10d9ab75
Tobias Merkle <tgamerkle@gmail.com>
2024-04-25 16:01:10 -0400

    port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
 // listen on PORT if it's set, otherwise use 8080
-app.listen(process.env.PORT || 8080, () => console.log(`blog/podcast app listening on ${PORT || 8080}`))
+app.listen(process.env.PORT || 8080, () => console.log(`blog/podcast app listening on ${process.env.PORT || 8080}`))
 
