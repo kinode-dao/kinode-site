1ad3109622cd292ad31d2a1287f9ece45abb22f1
Tobias Merkle <tobias@branchmessenger.com>
2024-06-13 13:51:46 -0400

    Revert "cave has a video now"
    This reverts commit 8bbae4f4bde05bb4fef2e36996644b6a2303b8db.

--- src/components/phonebook/Cave.tsx
+++ src/components/phonebook/Cave.tsx
@@ -3,23 +3,13 @@ import Row from "../spacing/Row"
 import { isMobileCheck } from "../../utils/dimensions"
 import classNames from "classnames"
 import './Cave.scss'
-import caveVid from '../../assets/img/kinodetree.webm'
 
 export const Cave = () => {
   const isMobile = isMobileCheck()
   return <Col className={classNames('page cave', { isMobile })}>
     <Row className="dimmer"></Row>
     <Row className="brighter shine"></Row>
-    <Row className='background'>
-      <video autoPlay muted loop playsInline className="bg-vid">
-        <source src={caveVid} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-      <video autoPlay muted loop playsInline className="centerpiece">
-        <source src={caveVid} type="video/webm" />
-        Your browser does not support the video tag.
-      </video>
-    </Row>
+    <Row className='background'></Row>
   </Col>
 }
 
